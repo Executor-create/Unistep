341a5128533fd1e00af703f2509d221f61798fd6
Executor <just4dotaalex@gmail.com>
2023-04-12 01:19:21 +0300

    feat: add route to notFound page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import NotFound from "../pages/NotFound/NotFound";
 import Signup from "../pages/Signup/Signup";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
   );
