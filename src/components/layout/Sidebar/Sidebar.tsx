6588e29b9b3d35c2c3357b98b27e7dbe7f09b6d2
Executor <just4dotaalex@gmail.com>
2023-04-19 01:58:44 +0300

    feat: added sidebar component

---
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiX } from "react-icons/hi";
+import SidebarData from "./SidebarData";
+import styles from "./Sidebar.module.css";
+
+type SidebarProps = {
+  open: boolean;
+  toggle: () => void;
+};
+
+const Sidebar = ({ open, toggle }: SidebarProps) => {
+  const items = SidebarData();
+
+  return (
+    <aside
+      className={`${styles.sidebar} ${open ? "" : styles.sidebar__hidden}`}
+    >
+      <div className={styles["sidebar__icon-wrapper"]}>
+        <HiX className={styles.sidebar__icon} onClick={toggle} />
+      </div>
+      <div>
+        {items.map(({ path, name }, index) => {
+          return (
+            <ul className={styles.sidebar__list}>
+              <Link className={styles.sidebar__link} to={path}>
+                <li className={styles.sidebar__item} key={index}>
+                  {name}
+                </li>
+              </Link>
+            </ul>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
