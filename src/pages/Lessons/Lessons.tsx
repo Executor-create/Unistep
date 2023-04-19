66a55d51b18b892210f676b2dd796d37cd9a720a
Executor <just4dotaalex@gmail.com>
2023-04-19 21:38:58 +0300

    feat: added Lessons component

---
+++ src/pages/Lessons/Lessons.tsx
@@ -0,0 +1,21 @@
+import Header from "../../components/layout/Header/Header";
+import Sidebar from "../../components/layout/Sidebar/Sidebar";
+import LessonsCardList from "./LessonsCardList/LessonsCardList";
+import useSidebarToggle from "../../hooks/useSidebarToggle";
+import styles from "./Lessons.module.css";
+
+const Lessons = () => {
+  const { isOpen, toggle: setOpen } = useSidebarToggle();
+
+  return (
+    <div className={styles.wrapper}>
+      <Header username="Geralt of Rivia" openSidebar={setOpen} />
+      <Sidebar open={isOpen} toggle={setOpen} />
+      <main className={styles.main}>
+        <LessonsCardList />
+      </main>
+    </div>
+  );
+};
+
+export default Lessons;
