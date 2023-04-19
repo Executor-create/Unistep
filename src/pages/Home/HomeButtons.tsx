c8c254a0d9387c4d1bc1416335c75f09f1251233
Executor <just4dotaalex@gmail.com>
2023-04-19 21:30:12 +0300

    feat: added HomeButtons component

---
+++ src/pages/Home/HomeButtons.tsx
@@ -0,0 +1,68 @@
+import Button from "../../components/common/Button";
+import { Link } from "react-router-dom";
+
+type Item = {
+  border: string;
+  color: string;
+  cursor: string;
+  fontSize?: string;
+  fontColor?: string;
+  height: string;
+  radius?: string;
+  text: string;
+  width: string;
+  path: string;
+};
+
+const buttons: Item[] = [
+  {
+    text: "CHOOSE YOUR DIRECTION",
+    height: "150px",
+    width: "300px",
+    border: "none",
+    radius: "40px",
+    color: "var(--secondary-color)",
+    fontSize: "20px",
+    fontColor: "#000000",
+    cursor: "pointer",
+    path: "/",
+  },
+  {
+    text: "ADDITIONAL LESSONS",
+    height: "150px",
+    width: "300px",
+    border: "none",
+    radius: "40px",
+    color: "var(--secondary-color)",
+    fontSize: "20px",
+    fontColor: "#000000",
+    cursor: "pointer",
+    path: "/lessons",
+  },
+  {
+    text: "CONTACT THE ADMINISTRATION",
+    height: "150px",
+    width: "300px",
+    border: "none",
+    radius: "40px",
+    color: "var(--secondary-color)",
+    fontSize: "20px",
+    fontColor: "#000000",
+    cursor: "pointer",
+    path: "/",
+  },
+];
+
+const HomeButtons = () => {
+  return (
+    <>
+      {buttons.map((button, index) => (
+        <Link to={button.path}>
+          <Button key={index} {...button} />
+        </Link>
+      ))}
+    </>
+  );
+};
+
+export default HomeButtons;
