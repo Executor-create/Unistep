3ade7a8a51c2eb623f123b71b0505d735ff6c5d1
Executor <just4dotaalex@gmail.com>
2023-05-03 00:24:01 +0300

    feat: added login user api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,4 @@
-import { FormValues } from "../types/types";
-
-export async function createUser<T>(data: FormValues): Promise<T> {
+export async function createUser<User>(data: User) {
   try {
     const response = await fetch(
       "https://c7v5sfccz6.execute-api.us-east-1.amazonaws.com/dev/signup",
@@ -13,9 +11,31 @@ export async function createUser<T>(data: FormValues): Promise<T> {
       }
     );
 
-    return await response.json();
+    await response.json();
   } catch (error) {
     console.log(error);
-    return { success: false, error: "An error occurred" } as T;
+  }
+}
+
+export async function loginUser<User>(data: User) {
+  try {
+    const response = await fetch(
+      "https://c7v5sfccz6.execute-api.us-east-1.amazonaws.com/dev/signin",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Invalid email or password");
+    }
+
+    await response.json();
+  } catch (error) {
+    throw error;
   }
 }
