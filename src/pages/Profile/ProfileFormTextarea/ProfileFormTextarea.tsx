08ee6feb1994c95172d936ff9e425c3fccfd8a71
Executor <just4dotaalex@gmail.com>
2023-05-04 00:52:13 +0300

    feat: added register to textarea

--- src/pages/Profile/ProfileFormTextarea/ProfileFormTextarea.tsx
+++ src/pages/Profile/ProfileFormTextarea/ProfileFormTextarea.tsx
@@ -1,10 +1,19 @@
 import styles from "./ProfileFormTextarea.module.css";
+import { UseFormRegister } from "react-hook-form";
+import { FormValues } from "../../../types/types";
 
-const ProfileFormTextarea = () => {
+type ProfileFormTextareaProps = {
+  register: UseFormRegister<FormValues>;
+};
+
+const ProfileFormTextarea = ({ register }: ProfileFormTextareaProps) => {
   return (
     <div className={styles["form__textarea-wrapper"]}>
       <label className={styles.form__label}>About</label>
-      <textarea className={styles.form__textarea}></textarea>
+      <textarea
+        className={styles.form__textarea}
+        {...register("about")}
+      ></textarea>
     </div>
   );
 };
