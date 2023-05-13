b1975ee6e649089ddf63d032a7cda654813f6fdf
Executor <just4dotaalex@gmail.com>
2023-05-13 19:38:13 +0300

    feat: update user type

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,12 +4,18 @@ export type FormValues = {
   username: string;
   phone: string;
   birthday: string;
-  avatar: string;
+  avatar_url: string;
   about: string;
 };
 
 export type User = {
+  user_id: string;
   username: string;
   email: string;
   password: string;
+  phone: string;
+  birthday: string;
+  avatar_url: string;
+  bio: string;
+  token: string;
 };
