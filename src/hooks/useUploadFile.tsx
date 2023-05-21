cd65c200f98de526e89ce4ea06276b9343fd97d9
Executor <just4dotaalex@gmail.com>
2023-05-21 22:39:42 +0300

    feat: added hook for upload file

--- src/hooks/useUploadFile.tsx
+++ src/hooks/useUploadFile.tsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
+import { UseFormSetValue } from "react-hook-form";
 
-const useUploadFile = () => {
-  const [selectedFile, setSelectedFile] = useState<string>();
+type UseUploadFileProps = {
+  setValue: UseFormSetValue<any>;
+};
 
-  const handleFileChange = (event: any) => {
-    const selectedFile = event.target.files?.[0];
-    setSelectedFile(URL.createObjectURL(selectedFile as File));
+const useUploadFile = ({ setValue }: UseUploadFileProps) => {
+  const [selectedFile, setSelectedFile] = useState<string | undefined>();
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      convertToBase64(file);
+    }
+  };
+
+  const convertToBase64 = (file: File) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      setSelectedFile(reader.result?.toString());
+      setValue("avatar_url", reader.result?.toString() || "");
+    };
   };
 
   return {
     selectedFile,
     handleFileChange,
+    convertToBase64,
   };
 };
 
