7a6672a4f07d77790317e7c4c872e53911a0148b
Executor <just4dotaalex@gmail.com>
2023-05-23 23:56:09 +0300

    feat: change sign in link to log out if user have token

--- src/components/layout/Sidebar/SidebarData.ts
+++ src/components/layout/Sidebar/SidebarData.ts
@@ -1,16 +1,20 @@
+import { getTokenData } from "../../../services/authService";
+
 type Item = {
   path: string;
   name: string;
 };
 
 const SidebarData = (): Item[] => {
+  const token = getTokenData();
+
   const items: Item[] = [
     { path: "/", name: "Main Page" },
     { path: "/profile", name: "Profile" },
     { path: "/journal", name: "Journal" },
     { path: "/chat", name: "Chat" },
     { path: "/schedule", name: "Schedule" },
-    { path: "/signin", name: "Sign in" },
+    { path: "/signin", name: token ? "Log out" : "Sign in" },
   ];
 
   return items;
